7cdb46724fc6d06244e9d0d31cbef20fe83cbaef
C1C0 <kpskola1002@gmail.com>
2021-11-07 17:57:46 +0100

    Adding variables for routes

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('posts');
+Route::get('/', function() {
+  return view('posts');
 });
 
-Route::get('post', function(){
-  return view("post");
+Route::get('post', function() {
+  return view("post", [
+          'post' => "<h1>hello world</h1>",
+      ]);
 });
