9b0610ab3a5271c0151efed247cd13eba7a3bc7b
C1C0 <kpskola1002@gmail.com>
2021-11-07 18:09:20 +0100

    Redirecting

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::get('post/{post}', function($slug) {
   $path = __DIR__ . "/../resources/posts/{$slug}.html";
 
   if (!file_exists($path)) {
-    abort(404);
+    return redirect('/');
   }
 
   $post = file_get_contents($path);
