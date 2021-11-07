0a32a4d2e6cd6d0f059981bedcf7cf78aa8a6bfd
C1C0 <kpskola1002@gmail.com>
2021-11-07 18:29:36 +0100

    Better to use abort() or exception, as redirecting is not job related to the Post class

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,13 +4,15 @@
 namespace App\Models;
 
 
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+
 class Post {
   public static function find($slug) {
 
     // using helper functions
     // also for "/app", "/resources" ...
     if (!file_exists($path = resource_path("posts/{$slug}.html"))) {
-      return redirect('/');
+      return new ModelNotFoundException();
     }
 
     return cache()->remember("post.{$slug}", now()->addHour(), fn() => file_get_contents($path));
