de20c2e87ed4841b4c84a6d74a415033e8b0609f
C1C0 <kpskola1002@gmail.com>
2021-11-08 20:37:34 +0100

    Layout - components - x-layout - x-slot

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,3 +1,14 @@
-<x-layout content="working">
+<x-layout>
+    <x-slot name="content">
+        @foreach($posts as $post)
+            <article class="{{ $loop->even ? 'class' : '' }}">
+                <a href="/posts/{{ $post->slug }}">
+                    <h1>{{ $post->title }}</h1>
 
+                </a>
+                <p>{{ $post->excerpt }}</p>
+
+            </article>
+        @endforeach
+    </x-slot>
 </x-layout>
