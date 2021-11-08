802372e4fea1c79c55ee7ca30367a8763ef497eb
C1C0 <kpskola1002@gmail.com>
2021-11-08 20:34:37 +0100

    Layout - components - x-layout content in parameter

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,14 +1,3 @@
-@extends('layout')
+<x-layout content="working">
 
-@section('content')
-    @foreach($posts as $post)
-        <article class="{{ $loop->even ? 'class' : '' }}">
-            <a href="/posts/{{ $post->slug }}">
-                <h1>{{ $post->title }}</h1>
-
-            </a>
-            <p>{{ $post->excerpt }}</p>
-
-        </article>
-    @endforeach
-@endsection
+</x-layout>
