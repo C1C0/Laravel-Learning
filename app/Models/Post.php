a4675177203681a362e9f30cb614dbc88c269d60
C1C0 <kpskola1002@gmail.com>
2021-11-08 20:51:09 +0100

    Clean up

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -19,15 +19,33 @@ class Post {
     $this->slug = $slug;
   }
 
+  /**
+   * Of all the blog posts, find the one with a slog that matches the one that was requested
+   * @param $slug
+   *
+   * @return mixed
+   */
   public static function find($slug) {
-    // Of all the blog posts, find the one with a slog that matches the one that was requested
-    $allPosts = static::all();
+    return static::all()->firstWhere('slug', $slug);
+  }
 
-    return $allPosts->firstWhere('slug', $slug);
+  /**
+   * Of all the blog posts, find the one with a slog that matches the one that was requested
+   * Fail on not found
+   * @param $slug
+   */
+  public static function findOrFail($slug) {
+    $post = static::find($slug);
+
+    if(empty($post)){
+      throw new ModelNotFoundException();
+    }
+
+    return $post;
   }
 
   public static function all(): Collection {
-    return cache()->rememberForever('posts.all', function(){
+    return cache()->rememberForever('posts.all', function() {
       return collect(File::files(resource_path("posts")))
           ->map(fn($file) => YamlFrontMatter::parseFile($file))
           ->map(fn($postData) => new Post($postData->title, $postData->body(), $postData->date, $postData->excerpt, $postData->slug))
