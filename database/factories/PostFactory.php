0e04b99c27487381bbe2dfca43a398aaaed4c84f
C1C0 <kpskola1002@gmail.com>
2021-11-09 20:18:27 +0100

    Fix factory

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -6,22 +6,20 @@ use App\Models\Category;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-class PostFactory extends Factory
-{
-    /**
-     * Define the model's default state.
-     *
-     * @return array
-     */
-    public function definition()
-    {
-        return [
-            'title' => $this->faker->sentence,
-            'excerpt' => $this->faker->sentence,
-            'body' => $this->faker->paragraph,
-            'slug' => $this->faker->slug,
-            'user_id' => User::factory(),
-            'category_id' => Category::factory(),
-        ];
-    }
+class PostFactory extends Factory {
+  /**
+   * Define the model's default state.
+   *
+   * @return array
+   */
+  public function definition() {
+    return [
+        'title' => $this->faker->sentence,
+        'excerpt' => $this->faker->sentence,
+        'body' => "<p>" . $this->faker->paragraph . "</p>",
+        'slug' => $this->faker->slug,
+        'user_id' => User::factory(),
+        'category_id' => Category::factory(),
+    ];
+  }
 }
