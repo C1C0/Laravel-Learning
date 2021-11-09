1dced127bb72b4a04c57163bcd6715415d796f54
C1C0 <kpskola1002@gmail.com>
2021-11-09 18:52:52 +0100

    Elequent Relationships - Post belongsTo category

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,10 +2,12 @@
     <x-slot name="content">
         @foreach($posts as $post)
             <article class="{{ $loop->even ? 'class' : '' }}">
-                <a href="/posts/{{ $post->slug }}">
-                    <h1>{{ $post->title }}</h1>
+                <h1>
+                    <a href="/posts/{{ $post->slug }}">{{ $post->title }}</a>
+                </h1>
 
-                </a>
+                <a href="#">{{ $post->category->name }}</a>
+
                 <p>{{ $post->excerpt }}</p>
 
             </article>
