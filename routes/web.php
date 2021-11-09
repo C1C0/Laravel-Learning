c00279efd030e5ce58214962185133d4638aebd5
C1C0 <kpskola1002@gmail.com>
2021-11-09 18:33:33 +0100

    Route Model Binding - using different localization mechanism

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,6 @@ Route::get('/', function() {
   ]);
 });
 
-Route::get('posts/{post}', function(Post $post) {
+Route::get('posts/{post:slug}', function(Post $post) { // Post::where('slug', $post)->firstOrFail()
   return view('post', ['post' => $post]);
 });
