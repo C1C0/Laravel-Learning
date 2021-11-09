a1539c62972ddd66f7c9da8b85f36a9a67000b68
C1C0 <kpskola1002@gmail.com>
2021-11-09 18:25:26 +0100

    Database - Transitioning post to eloquent model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -1,56 +1,11 @@
 <?php
 
-
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Illuminate\Support\Collection;
-use Illuminate\Support\Facades\File;
-use Spatie\YamlFrontMatter\YamlFrontMatter;
-
-class Post {
-
-  public function __construct(public $title, public $body, public $date, public $excerpt, public $slug) {
-    $this->title = $title;
-    $this->body = $body;
-    $this->date = $date;
-    $this->excerpt = $excerpt;
-    $this->slug = $slug;
-  }
-
-  /**
-   * Of all the blog posts, find the one with a slog that matches the one that was requested
-   * @param $slug
-   *
-   * @return mixed
-   */
-  public static function find($slug) {
-    return static::all()->firstWhere('slug', $slug);
-  }
-
-  /**
-   * Of all the blog posts, find the one with a slog that matches the one that was requested
-   * Fail on not found
-   * @param $slug
-   */
-  public static function findOrFail($slug) {
-    $post = static::find($slug);
-
-    if(empty($post)){
-      throw new ModelNotFoundException();
-    }
-
-    return $post;
-  }
-
-  public static function all(): Collection {
-    return cache()->rememberForever('posts.all', function() {
-      return collect(File::files(resource_path("posts")))
-          ->map(fn($file) => YamlFrontMatter::parseFile($file))
-          ->map(fn($postData) => new Post($postData->title, $postData->body(), $postData->date, $postData->excerpt, $postData->slug))
-          ->sortByDesc('date');
-    });
-  }
-
+class Post extends Model
+{
+    use HasFactory;
 }
