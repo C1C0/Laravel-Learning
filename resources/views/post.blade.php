f178459c3d4343100690193e851944f99f738bee
C1C0 <kpskola1002@gmail.com>
2021-11-10 09:12:28 +0100

    Dividing responsibility between more files in blade

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,11 +1,11 @@
 <x-layout>
-    <x-slot name="content">
-        <article>
-            <h1>{{ $post->title }}</h1>
-            By <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> for <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
-            <div>{!! $post->body !!}</div>
-        </article>
+{{--    <x-slot name="content">--}}
+{{--        <article>--}}
+{{--            <h1>{{ $post->title }}</h1>--}}
+{{--            By <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> for <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>--}}
+{{--            <div>{!! $post->body !!}</div>--}}
+{{--        </article>--}}
 
-        <a href="/">Go back</a>
-    </x-slot>
+{{--        <a href="/">Go back</a>--}}
+{{--    </x-slot>--}}
 </x-layout>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,16 +1,36 @@
 <x-layout>
-    <x-slot name="content">
-        @foreach($posts as $post)
-            <article class="{{ $loop->even ? 'class' : '' }}">
-                <h1>
-                    <a href="/posts/{{ $post->slug }}">{{ $post->title }}</a>
-                </h1>
 
-                By <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> for <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
+    @include("_posts-header")
 
-                <p>{{ $post->excerpt }}</p>
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        <x-post-featured-card />
 
-            </article>
-        @endforeach
-    </x-slot>
+{{--        <div class="lg:grid lg:grid-cols-2">--}}
+{{--            <x-post-card/>--}}
+{{--            <x-post-card/>--}}
+{{--        </div>--}}
+
+{{--        <div class="lg:grid lg:grid-cols-3">--}}
+{{--            <x-post-card/>--}}
+{{--            <x-post-card/>--}}
+{{--            <x-post-card/>--}}
+{{--        </div>--}}
+
+
+
+
+    {{--    <x-slot name="content">--}}
+    {{--        @foreach($posts as $post)--}}
+    {{--            <article class="{{ $loop->even ? 'class' : '' }}">--}}
+    {{--                <h1>--}}
+    {{--                    <a href="/posts/{{ $post->slug }}">{{ $post->title }}</a>--}}
+    {{--                </h1>--}}
+
+    {{--                By <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> for <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>--}}
+
+    {{--                <p>{{ $post->excerpt }}</p>--}}
+
+    {{--            </article>--}}
+    {{--        @endforeach--}}
+    {{--    </x-slot>--}}
 </x-layout>
