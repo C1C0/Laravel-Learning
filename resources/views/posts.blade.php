0fd40707b7b44e11696b7d8aecbb0fb698173fd4
C1C0 <kpskola1002@gmail.com>
2021-11-10 10:18:20 +0100

    Dynamic posts displaying - skipping the first item as has different layout

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -6,7 +6,7 @@
         <x-post-featured-card :post="$posts[0]" />
 
         <div class="lg:grid lg:grid-cols-2">
-            @foreach($posts as $post)
+            @foreach($posts->skip(1) as $post)
                 <x-post-card :post="$post"/>
             @endforeach
         </div>
