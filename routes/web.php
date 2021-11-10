ca86a47772ac55d45ed5dad78efd668b69e9d557
C1C0 <kpskola1002@gmail.com>
2021-11-10 21:02:48 +0100

    Different approach of selecting style for selected categories - using request()->is() and request()->routeIs('route')

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::get('/', function() {
       "posts" => Post::latest()->get(),
       'categories' => Category::all(),
   ]);
-});
+})->name('home');
 
 Route::get('posts/{post}', function(Post $post) { // Post::where('slug', $post)->firstOrFail()
   return view('post', ['post' => $post]);
@@ -35,7 +35,7 @@ Route::get('categories/{category:slug}', function(Category $category) {
       'categories' => Category::all(),
       'currentCategory' => $category,
   ]);
-});
+})->name('category');
 
 Route::get('authors/{author:username}', function(User $author) {
   return view('posts', [
