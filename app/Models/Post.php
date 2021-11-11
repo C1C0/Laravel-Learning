ae8398d664e73e2a719fe41a68b16ab4ad660ed1
C1C0 <kpskola1002@gmail.com>
2021-11-11 10:22:43 +0100

    Search - Cleaner way - Refactoring the Filter query scope function as well

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -24,10 +24,8 @@ class Post extends Model {
    * @param $query
    */
   public function scopeFilter($query, array $filters) {
-    if ($filters[Config::get('constants.GET_REQUEST.SEARCH')] ?? false) {
-      $query->where('title', 'like', "%" . request(Config::get('constants.GET_REQUEST.SEARCH')) . "%")
-          ->orWhere('body', 'like', "%" . request(Config::get('constants.GET_REQUEST.SEARCH')) . "%");
-    }
+    $query->when($filters[Config::get('constants.GET_REQUEST.SEARCH')] ?? FALSE, fn($query) => $query->where('title', 'like', "%" . request(Config::get('constants.GET_REQUEST.SEARCH')) . "%")
+        ->orWhere('body', 'like', "%" . request(Config::get('constants.GET_REQUEST.SEARCH')) . "%"));
   }
 
   public function category() {
