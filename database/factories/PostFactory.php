4548c50440ab36ef2d23b364da17927f290c73ab
C1C0 <kpskola1002@gmail.com>
2021-11-11 09:40:29 +0100

    Clean up

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -15,8 +15,8 @@ class PostFactory extends Factory {
   public function definition() {
     return [
         'title' => $this->faker->sentence,
-        'excerpt' => $this->faker->sentence,
-        'body' => "<p>" . $this->faker->paragraph . "</p>",
+        'excerpt' => '<p>' . implode("</p><p>", $this->faker->paragraphs(2)) . '</p>',
+        'body' => '<p>' . implode("</p><p>", $this->faker->paragraphs(7)) . '</p>',
         'slug' => $this->faker->slug,
         'user_id' => User::factory(),
         'category_id' => Category::factory(),
