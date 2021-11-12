c14350c68cd7bff67e921123a92664f35272f389
C1C0 <kpskola1002@gmail.com>
2021-11-12 18:04:50 +0100

    Extract panel

--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -1,23 +1,25 @@
 @props(['comment'])
 
-<article class="flex bg-gray-100 border border-gray-200 p-6 rounded-xl space-x-5">
+<x-panel class="bg-gray-50">
+    <article class="flex space-x-5">
 
-    <div class="flex-shrink-0">
-        <img src="https://i.pravatar.cc/100?u={{ $comment->id }}" alt="" width="60" height="60" class="rounded-xl">
-    </div>
+        <div class="flex-shrink-0">
+            <img src="https://i.pravatar.cc/100?u={{ $comment->id }}" alt="" width="60" height="60" class="rounded-xl">
+        </div>
 
-    <div>
-        <header class="mb-4">
-            <h3 class="font-bold">{{ $comment->author->username }}</h3>
+        <div>
+            <header class="mb-4">
+                <h3 class="font-bold">{{ $comment->author->username }}</h3>
 
-            <p class="text-xs">
-                Posted
-                <time>{{ $comment->created_at->diffForHumans()}}</time>
-            </p>
-        </header>
+                <p class="text-xs">
+                    Posted
+                    <time>{{ $comment->created_at->diffForHumans()}}</time>
+                </p>
+            </header>
 
 
-        {{ $comment->body }}
+            {{ $comment->body }}
 
-    </div>
-</article>
+        </div>
+    </article>
+</x-panel>
