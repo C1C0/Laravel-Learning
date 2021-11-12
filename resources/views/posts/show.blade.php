55c27edf0f718b940978823893f210327da39ddc
C1C0 <kpskola1002@gmail.com>
2021-11-12 18:00:42 +0100

    Place comment - layout

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -56,8 +56,32 @@
                 </div>
 
                 <section class="col-span-8 col-start-5 mt-10 space-y-6">
+                    <form action="/" method="post" class="border border-gray-200 p-6 rounded-xl">
+                        @csrf
+
+                        <header class="flex items-center">
+                            <img src="https://i.pravatar.cc/100?u={{ auth()->id() }}" alt="" width="40" height="40"
+                                 class="rounded-full">
+
+                            <h2 class="ml-4">Want to participate ?</h2>
+                        </header>
+
+                        <div>
+                            <textarea name="body" id="body" cols="30" rows="5"
+                                      placeholder="Quick, thing of something..."
+                                      class="w-full mt-5 text-sm focus:outline-none focus:ring"></textarea>
+                        </div>
+
+                        <div class="flex justify-end mt-6 border-t borderg-gray-200 pt-6">
+                            <button type="submit"
+                                    class="bg-blue-500 text-white uppercase font-semibold text-xs py-2 px-10 rounded-2xl hover:bg-blue-600">
+                                Post
+                            </button>
+                        </div>
+                    </form>
+
                     @foreach($post->comments as $comment)
-                        <x-post-comment :comment="$comment" />
+                        <x-post-comment :comment="$comment"/>
                     @endforeach
                 </section>
             </article>
