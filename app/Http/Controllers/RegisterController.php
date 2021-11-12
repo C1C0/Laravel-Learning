9de015edaf9308170aaf9ba1b8a37d8b39e66e86
C1C0 <kpskola1002@gmail.com>
2021-11-12 15:18:05 +0100

    Authentification - registration (creating view, new Controller, updating Routes)

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+
+class RegisterController extends Controller
+{
+    public function create()
+    {
+        return view('register.create');
+    }
+
+    public function store()
+    {
+        // Redirects to previous page if condition not met
+        $attributes = request()->validate(
+            [
+                'name' => ['required'], // new syntax,
+                'username' => 'required|max:25', // old syntax
+                'email' => ['required', 'email'],
+                'password' => ['required', 'min:7', 'max:255'],
+            ]
+        );
+
+        $user = new User();
+        $user->name = $attributes['name'];
+        $user->username = $attributes['username'];
+        $user->email = $attributes['email'];
+        $user->password = $attributes['password'];
+        $user->save();
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,13 @@
 <?php
 
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\RegisterController;
 use App\Models\Category;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('posts/{post}', [PostController::class, 'show']);
+
+Route::get('register', [RegisterController::class, 'create']);
+Route::post('register', [RegisterController::class, 'store']);
