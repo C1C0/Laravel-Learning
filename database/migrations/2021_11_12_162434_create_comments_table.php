3bf80f38b3f946bcc60fbdae274906c0b03e2505
C1C0 <kpskola1002@gmail.com>
2021-11-12 17:32:24 +0100

    Comments - Foreign constraints - shorthand - same functionality

--- database/migrations/2021_11_12_162434_create_comments_table.php
+++ database/migrations/2021_11_12_162434_create_comments_table.php
@@ -15,14 +15,11 @@ class CreateCommentsTable extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('post_id');
             $table->unsignedBigInteger('user_id');
             $table->text('body');
             $table->timestamps();
 
-//            $table->foreignId('post_id')->references('id')->on('posts')->onDelete('cascade');
-            $table->foreignId('post_id')->references('id')->on('posts')->cascadeOnDelete();
-
+            $table->foreignId('post_id')->constrained();
         });
     }
 
