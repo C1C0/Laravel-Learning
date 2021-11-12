8c8abb5b5aa47c32d9f39c578dfc83e5e06b480b
C1C0 <kpskola1002@gmail.com>
2021-11-12 18:36:38 +0100

    Place comment - instead of writing fillable or guarded, use ServiceProvider

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -9,8 +9,6 @@ class Comment extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['user_id', 'body'];
-
     public function post(){
         return $this->belongsTo(Post::class);
     }
