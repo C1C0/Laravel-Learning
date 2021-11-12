a3cdb4aa0384d8ec6165d866f345601138ab4abf
C1C0 <kpskola1002@gmail.com>
2021-11-12 17:36:57 +0100

    HAVE TO ADD CASCADEONDELETE ON CONSTRAINT

--- database/migrations/2021_11_12_162434_create_comments_table.php
+++ database/migrations/2021_11_12_162434_create_comments_table.php
@@ -18,8 +18,8 @@ class CreateCommentsTable extends Migration
             $table->text('body');
             $table->timestamps();
 
-            $table->foreignId('post_id')->constrained();
-            $table->foreignId('user_id')->constrained();
+            $table->foreignId('post_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
         });
     }
 
